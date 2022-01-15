5c7ee7acd512585fe720f5586c5195e8cdaf75c2
Akash Verma <akashlucky18@gmail.com>
2022-01-16 02:49:49 +0530

    added homepage and app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 # Now define the basic route / and its corresponding request handler:
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html')
 # Next, check if the executed file is the main program and run the app:
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
 
